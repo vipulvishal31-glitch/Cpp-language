4c49fa8efd6e1367a3a648653631e000e3775489
Vipul singh <vipulvishal31@gmail.com>
2026-03-24 20:56:54 +0530

    calculator

---
+++ calc.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int calc(){
+int a,b;
+cout<<"enter the value a&b";
+cin>>a>>b;
+cout<<a+b<<" "<<a-b<<" "<<a*b<<" "<<a/b<< " "<<a%b;
+}
+int main(){
+    calc();
+}
